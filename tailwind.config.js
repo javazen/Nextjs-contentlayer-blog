1dc4b9136776c9c677966292361475ea0f77340c
Sam Ragucci <samr742@gmail.com>
2024-01-16 15:24:05 -0800

    add our colors to Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,12 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        dark: "#1b1b1b",
+        light: "#fff",
+        accent: "#7B00D3", 
+        accentDark: "#ffdb4d",
+        gray: "#747474",
       },
     },
   },
