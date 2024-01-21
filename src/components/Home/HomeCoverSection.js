f3e9bcc816f60611aa8267918c8f7f11d046df56
Sam Ragucci <samr742@gmail.com>
2024-01-20 16:44:50 -0800

    first cover section showing image

--- src/components/Home/HomeCoverSection.js
+++ src/components/Home/HomeCoverSection.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
+import { cx, sortBlogs } from '@/src/utils/index';
+
 
 const HomeCoverSection = ({blogs}) => {
+    const sortedBlogs = sortBlogs(blogs);
+    const latestBlog = sortedBlogs[0];
+    console.log(latestBlog.image.filePath);
   return (
-    <div>HomeCoverSection</div>
+    <article>
+      <Image src={latestBlog.image.filePath.replace("../public", "")} 
+      placeholder='blur'
+      blurDataURL={latestBlog.image.blurhashDataUrl}
+      alt={latestBlog.title}
+      fill
+      className='w-full h-full object-center object-cover rounded-3xl '
+      />
+
+    </article>
   )
 }
 
