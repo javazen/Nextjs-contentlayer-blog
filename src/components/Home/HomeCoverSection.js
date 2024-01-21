306c8dc69dc5d00876f30c0ebc44ed14b13acb2c
Sam Ragucci <samr742@gmail.com>
2024-01-20 17:15:00 -0800

    text for the cover image
    Link to category, text for title and description

--- src/components/Home/HomeCoverSection.js
+++ src/components/Home/HomeCoverSection.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 import { cx, sortBlogs } from '@/src/utils/index';
 
 
@@ -17,8 +18,19 @@ const HomeCoverSection = ({blogs}) => {
       fill
       className='w-full h-full object-center object-cover rounded-3xl '
       />
-      <div className='absolute top-0 left-0 bottom-0 right-0 h-full bg-gradient-to-b from-transparent from-0% to-dark/90 rounded-3xl z-10' />
+      <div className='absolute top-0 left-0 bottom-0 right-0 h-full bg-gradient-to-b from-transparent from-0% to-dark/90 rounded-3xl z-0' />
 
+      <div className='w-3/4 p-16 flex flex-col items-start justify-center z-0 text-light'>
+        <Link href={`/categories/${latestBlog.tags[0]}`}>
+          {latestBlog.tags[0]}
+        </Link>
+        <h1>
+          {latestBlog.title}
+        </h1>
+        <p>
+          {latestBlog.description}
+        </p>
+      </div>
     </article>
 
     </div>
