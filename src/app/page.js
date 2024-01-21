dbc3fa91915f21ce9e4feca87126a4e928414a0c
Sam Ragucci <samr742@gmail.com>
2024-01-20 16:04:09 -0800

    log blogs that get created

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,8 @@
+import { allBlogs } from '../../.contentlayer/generated'
 import Image from 'next/image'
 
 export default function Home() {
+    // console.log(allBlogs);
   return (
     <main className="flex flex-col items-center justify-center">
       Ciao, Mondo!
