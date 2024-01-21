b2c5fbaca85c3774b0bb293d59e3ddd4ebdb1d97
Sam Ragucci <samr742@gmail.com>
2024-01-20 17:43:49 -0800

    add hover effect

--- src/components/Elements/Tag.js
+++ src/components/Elements/Tag.js
@@ -5,7 +5,7 @@ import { cx, sortBlogs } from '@/src/utils/index';
 
 const Tag = ({link="#", name, ...props}) => {
   return (
-    <Link href={link} className={cx("inline-block", props.className)}>
+    <Link href={link} className={cx("inline-block py-3 px-10 bg-dark text-light rounded-full capitalize font-semibold border-2 border-solid border-light hover:scale-105 transition-all ease duration-200", props.className)}>
         {name}
     </Link>
   )
