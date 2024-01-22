a2096b6a07ba96094ff5874c3e37627cb2280ec6
Sam Ragucci <samr742@gmail.com>
2024-01-22 15:14:41 -0800

    Make nav bar float on top when scrolling

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,7 +7,7 @@ const Header = () => {
     <header className="w-full p-4 px-10 flex items-center justify-between">
         <Logo />
         <nav className="w-max py-3 px-8 border border-solid rounded-full font-medium capitalize flex items-center 
-        fixed top-6 right-1/2 translate-x-1/2 bg-light/80 backdrop-blur-sm">
+        fixed top-6 right-1/2 translate-x-1/2 bg-light/80 backdrop-blur-sm z-50">
         <Link href="/" className="mr-2">Home</Link>
         <Link href="/about" className="mx-2">About</Link>
         <Link href="/contact" className="mx-2">Contact</Link>
