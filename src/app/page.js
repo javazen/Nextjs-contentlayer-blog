3602577c6cb87a42ace2ccad26e4900f855f83c9
Sam Ragucci <samr742@gmail.com>
2024-01-22 15:15:19 -0800

    add Recent Posts section

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import { allBlogs } from 'contentlayer/generated'
 import Image from 'next/image'
 import HomeCoverSection from '@/src/components/Home/HomeCoverSection'
 import FeaturedPosts from '@/src/components/Home/FeaturedPosts'
+import RecentPosts from '@/src/components/Home/RecentPosts'
 
 export default function Home() {
     // console.log(allBlogs);
@@ -9,6 +10,7 @@ export default function Home() {
     <main className="flex flex-col items-center justify-center">
       <HomeCoverSection blogs={allBlogs} />
       <FeaturedPosts blogs={allBlogs} />
+      <RecentPosts blogs={allBlogs} />
     </main>
   )
 }
