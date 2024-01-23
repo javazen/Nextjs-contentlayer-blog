4f7c503b43f1e9c1f5255bafcc05410c00640d02
Sam Ragucci <samr742@gmail.com>
2024-01-23 10:02:35 -0800

    add index as key
    in the map from array of blogs to list of articles

--- src/components/Home/RecentPosts.js
+++ src/components/Home/RecentPosts.js
@@ -17,7 +17,7 @@ const RecentPosts = ({blogs}) => {
         <div className='grid grid-cols-3 grid-rows-3 gap-16 mt-16'>
             {
                 sortedBlogs.slice(5, 11).map((blog, index) => {
-                    return <article className='col-span-1 row-span-1 relative'>
+                    return <article key={index} className='col-span-1 row-span-1 relative'>
                         <BlogLayoutThree blog={blog} />
                     </article>
                 })
