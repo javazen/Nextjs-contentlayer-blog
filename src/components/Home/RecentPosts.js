e53729487d6fcc564fa875f08f0f079e4ce27b54
Sam Ragucci <samr742@gmail.com>
2024-01-24 18:23:17 -0800

    remove whitespace at bottom
    change grid-rows-3 to -2

--- src/components/Home/RecentPosts.js
+++ src/components/Home/RecentPosts.js
@@ -14,7 +14,7 @@ const RecentPosts = ({blogs}) => {
             <Link href="/categories/all" className='inline-block font-medium text-accent underline underline-offset-2 text-lg'>view all</Link>
         </div>
 
-        <div className='grid grid-cols-3 grid-rows-3 gap-16 mt-16'>
+        <div className='grid grid-cols-3 grid-rows-2 gap-16 mt-16'>
             {
                 sortedBlogs.slice(5, 11).map((blog, index) => {
                     return <article key={index} className='col-span-1 row-span-1 relative'>
