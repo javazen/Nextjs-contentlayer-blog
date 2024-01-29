4085458c98efe59fdaf3b4782a6c21bcce590d97
Sam Ragucci <samr742@gmail.com>
2024-01-28 19:52:02 -0800

    custom style for blockquote and li

--- src/components/Blog/RenderMdx.js
+++ src/components/Blog/RenderMdx.js
@@ -10,7 +10,15 @@ const mdxComponents = {
 const RenderMdx = ({blog}) => {
     const MDXContent = useMDXComponent(blog.body.code)
   return (
-    <div className='col-span-8 font-in'>
+    <div className='col-span-8 font-in prose prose-lg max-w-max 
+    prose-blockquote:bg-accent/20
+    prose-blockquote:p-2
+    prose-blockquote:px-6
+    prose-blockquote:border-accent
+    prose-blockquote:not-italic
+    prose-blockquote:rounded-r-lg
+    prose-li:marker:text-accent
+    '>
         <MDXContent components={mdxComponents}/>
     </div>
   )
