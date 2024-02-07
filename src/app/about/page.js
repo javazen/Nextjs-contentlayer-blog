ea830d0b8a5d8ca707f05575eb72837a1a7cd252
Sam Ragucci <samr742@gmail.com>
2024-02-06 21:56:16 -0800

    add Skills section

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,6 +1,14 @@
 import AboutCoverSection from '@/src/components/About/AboutCoverSection'
-
+import Skills from '@/src/components/About/Skills'
+import Link from 'next/link'
 
 export default function About() {
-    return <><AboutCoverSection /></>
+    return <>
+    <AboutCoverSection />
+    <Skills />
+    <h2 className='mt-8 mx-20 font-semibold text-2xl text-dark self-start'>
+    Have a project in mind? Reach out to me 📞 from <Link href="/contact" className="!underline 
+    underline-offset-2">here</Link> and let's make it happen.
+    </h2>
+    </>
 }
---
+++ src/components/About/Skills.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const SkillList = [
+    "next.js",
+    "tailwind css",
+    "figma",
+    "javaScript",
+    "web design",
+    "Gatsby.js",
+    "strapi",
+    "firebase",
+    "generative AI",
+    "wireframing",
+    "SEO",
+    "framer motion",
+    "sanity",
+  ];
+  
+const Skills = () => {
+  return (
+    <section className='w-full flex flex-col p-20 border-b-2 border-solid border-dark text-dark'>
+        <span className='font-semibold text-4xl text-accent'>
+            I'm comfortable in...
+            <ul className='flex flex-wrap mt-8 justify-start'>
+            {
+                SkillList.map((item, index) => {
+                    return <li key={index} className='font-semibold inline-block capitalize text-2xl py-5 px-12 
+                    border-2 border-solid border-dark rounded mr-6 mb-6 
+                    hover:scale-105 transition-all ease duration-200 cursor-pointer'>{item}</li>
+                })
+            }
+            </ul>
+        </span>
+    </section>
+  )
+}
+
+export default Skills
