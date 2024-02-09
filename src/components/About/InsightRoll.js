19113df449c3b1e5290f903344796a442c26fafc
Sam Ragucci <samr742@gmail.com>
2024-02-09 09:24:15 -0800

    animate the insight roll

--- src/components/About/InsightRoll.js
+++ src/components/About/InsightRoll.js
@@ -3,7 +3,7 @@ import React from 'react'
 const InsightRoll = ({insights}) => {
   return (
     <div className='w-full bg-accent text-light whitespace-nowrap overflow-hidden'>
-        <div className='w-full py-3 flex items-center justify-center capitalize font-semibold tracking-wider text-base'>
+        <div className='animate-roll w-full py-3 flex items-center justify-center capitalize font-semibold tracking-wider text-base'>
             {
                 insights.map(text => <div>{text}<span className='px-4'>|</span></div>)
             }
--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,15 @@ module.exports = {
       fontFamily: {
         mr: ["var(--font-mr)"],
         in: ["var(--font-in)"]
+      },
+      animation: {
+        roll: "roll 24s linear infinite"
+      },
+      keyframes: {
+        roll: {
+          "0%": {transform: "translateX(100%)"},
+          "100%": {transform: "translateX(-100%)"}
+        }
       }
     },
   },
