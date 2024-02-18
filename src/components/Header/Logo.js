dbee2b9ecd70cf02b2c828d62690113a1103f3dd
Sam Ragucci <samr742@gmail.com>
2024-02-17 22:22:51 -0800

    responsive design
    light colors where needed for text, border, convert header to hamburger menu when <640px wide

--- src/components/Header/Logo.js
+++ src/components/Header/Logo.js
@@ -4,11 +4,11 @@ import profileImg from "@/public/profile-img.png"
 
 const Logo = () => {
   return (
-    <Link href="/" className="flex items-center text-dark">
-        <div className="w-16 rounded-full overflow-hidden border border-solid border-dark mr-4">
+    <Link href="/" className="flex items-center text-dark dark:text-light">
+        <div className="w-16 rounded-full overflow-hidden border border-solid border-dark dark:border-light mr-4">
             <Image src={profileImg} alt="Javazen" className="w-full h-auto rounded-full" />
         </div>
-        <span className="font-bold text-xl">Javazen</span>
+        <span className="font-bold dark:font-semibold text-xl">Javazen</span>
     </Link>
   )
 }
