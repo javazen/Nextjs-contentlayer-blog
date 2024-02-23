298a6b809a201fd4a14a792f22be8fe047fc8cd2
Sam Ragucci <samr742@gmail.com>
2024-02-23 15:56:26 -0800

    add sitemap.xml

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+const siteMetadata = require("./src/utils/siteMetadata");
+
+
+module.exports = {
+    siteUrl: siteMetadata.siteUrl,
+    generateRobotsTxt: true,
+    sitemapSize: 7000,
+  }
--- src/components/Blog/Categories.js
+++ src/components/Blog/Categories.js
@@ -8,7 +8,7 @@ const Categories = ({categories, currentSlug}) => {
   return (
     <div className='px-0 md:px-10 sxl:px-20 mt-10 text-dark dark:text-light 
     border-t-2 border-b-2 border-solid border-dark dark:border-light
-    py-4 items-start flex flex-wrap font-medium mx-10'>
+    py-4 items-start flex flex-wrap font-medium mx-5 md:mx-10'>
         {categories.map(cat => 
             <Category 
             key={cat} 
