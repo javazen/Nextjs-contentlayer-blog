e8703f6657c1131c766611c542b61413c11b0ccb
Sam Ragucci <samr742@gmail.com>
2024-02-26 20:42:36 -0800

    Add code to show correct Sun/Moon icon

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,6 +5,7 @@ import { LinkedinIcon, TwitterIcon, GithubIcon, DribbbleIcon, SunIcon, MoonIcon 
 import siteMetadata from "@/src/utils/siteMetadata";
 import { useThemeSwitch } from "../Hooks/useThemeSwitch";
 import { useState } from "react";
+import { cx } from "@/src/utils";
 
 const Header = () => {
 
@@ -17,7 +18,7 @@ const Header = () => {
 
   return (
     <header className="w-full p-4  px-5 sm:px-10 flex items-center justify-between">
-      <Logo /> rounded transition-all ease duration-200 
+      <Logo /> 
       <button className="inline-block z-50 sm:hidden rounded transition-all ease duration-200" onClick={toggle}>
         <div className="w-6 cursor-pointer transition-all ease duration-300" >
           <div className="relative">
@@ -54,8 +55,13 @@ const Header = () => {
         <Link href="/" className="mr-2">Home</Link>
         <Link href="/about" className="mx-2">About</Link>
         <Link href="/contact" className="mx-2">Contact</Link>
-        <button>
-          <SunIcon onClick={() => setMode(mode === 'light' ? 'dark' : 'light')} />
+        <button onClick={() => setMode(mode === 'light' ? 'dark' : 'light')}
+        className={cx("w-6 h-6 ease ml-2 flex items-center justify-center rounded-full p-1", 
+        mode === "light" ? "bg-dark text-light" : "bg-light text-dark")}
+        >
+          {
+            mode === "light" ? <MoonIcon className={"fill-dark"} /> : <SunIcon className={"fill-light"} />
+          }
         </button>
       </nav>
       
@@ -65,8 +71,13 @@ const Header = () => {
         <Link href="/" className="mr-2">Home</Link>
         <Link href="/about" className="mx-2">About</Link>
         <Link href="/contact" className="mx-2">Contact</Link>
-        <button>
-          <SunIcon onClick={() => setMode(mode === 'light' ? 'dark' : 'light')} />
+        <button onClick={() => setMode(mode === 'light' ? 'dark' : 'light')}
+        className={cx("w-6 h-6 ease ml-2 flex items-center justify-center rounded-full p-1", 
+        mode === "light" ? "bg-dark text-light" : "bg-light text-dark")}
+        >
+          {
+            mode === "light" ? <MoonIcon className={"fill-dark"} /> : <SunIcon className={"fill-light"} />
+          }
         </button>
       </nav>
       
