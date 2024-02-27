f995d6db69a91358d285aaab9557afc78f481e95
Sam Ragucci <samr742@gmail.com>
2024-02-27 10:25:27 -0800

    point to javazen info and vercel deploy
    and to generic twitter, facebook, dribbble

--- src/utils/siteMetadata.js
+++ src/utils/siteMetadata.js
@@ -1,19 +1,19 @@
 const siteMetadata = {
-    title: 'Next.js Blog With Tailwind CSS and Contentlayer',
-    author: 'CodeBucks',
+    title: 'Next.js Blog With Tailwind CSS and Contentlayer, sr version',
+    author: 'Javazen',
     headerTitle: 'Next.js Blog',
     description: 'A blog created with Next.js, Tailwind.css and contentlayer.',
     language: 'en-us',
     theme: 'system', // system, dark or light
-    siteUrl: 'https://create-blog-with-nextjs.vercel.app', // your website URL
+    siteUrl: 'https://nextjs-contentlayer-blog-sr.vercel.app/', // your website URL
     siteLogo: '/logo.png',
     socialBanner: '/social-banner.png', // add social banner in the public folder
-    email: 'codebucks27@gmail.com', 
-    github: 'https://github.com/codebucks27',
-    twitter: 'https://twitter.com/code_bucks',
+    email: 'samr@threedgraphics.com', 
+    github: 'https://github.com/javazen',
+    twitter: 'https://twitter.com',
     facebook: 'https://facebook.com',
-    youtube: 'https://youtube.com/codebucks',
-    linkedin: 'https://www.linkedin.com/in/codebucks/',
+    youtube: 'https://youtube.com',
+    linkedin: 'https://www.linkedin.com/in/sam-ragucci-7856b71/',
     dribbble: 'https://www.dribbble.com',
     locale: 'en-US',
 }
