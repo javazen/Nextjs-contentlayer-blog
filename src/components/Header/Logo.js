813c4c963120e6253058fd00b071c4437fedf038
Sam Ragucci <samr742@gmail.com>
2024-02-26 20:18:05 -0800

    gray border for dark mode

--- src/components/Header/Logo.js
+++ src/components/Header/Logo.js
@@ -5,7 +5,7 @@ import profileImg from "@/public/profile-img.png"
 const Logo = () => {
   return (
     <Link href="/" className="flex items-center text-dark dark:text-light">
-        <div className="w-16 rounded-full overflow-hidden border border-solid border-dark dark:border-light mr-4">
+        <div className="w-16 rounded-full overflow-hidden border border-solid border-dark dark:border-gray mr-4">
             <Image 
             src={profileImg} 
             alt="Javazen" 
